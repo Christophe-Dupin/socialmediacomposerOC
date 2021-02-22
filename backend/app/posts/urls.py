63403cf03a4a78834a20093b0685fb68018467db
Christophe Dupin <mail.christophe.dupin@gmail.com>
2021-02-22 12:58:03 +0100

    change url name

--- backend/app/posts/urls.py
+++ backend/app/posts/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("home/", views.home, name="home"),
-    path("queue/", views.all_posts_queue, name="all_posts"),
+    path("queue/", views.all_posts_queue, name="all_posts_queue"),
     path("history/", views.all_posts_send, name="all_posts_send"),
 ]
