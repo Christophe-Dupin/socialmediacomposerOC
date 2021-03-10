01911ec524b81fc38956c9e71c06e82392c99406
Christophe Dupin <mail.christophe.dupin@gmail.com>
2021-03-10 21:56:22 +0100

    change scope facebook

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -156,7 +156,7 @@ EMAIL_USE_TLS = True
 SOCIALACCOUNT_PROVIDERS = {
     "facebook": {
         "METHOD": "oauth2",
-        "SCOPE": ["email", "public_profile", "user_friends"],
+        "SCOPE": ["email", "public_profile"],
         "AUTH_PARAMS": {"auth_type": "reauthenticate"},
         "FIELDS": [
             "id",
