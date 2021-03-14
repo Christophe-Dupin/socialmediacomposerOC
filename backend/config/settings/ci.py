b3898035c0a2d7862e6e8d65be6727865c848ec0
Christophe Dupin <mail.christophe.dupin@gmail.com>
2021-03-14 11:23:07 +0100

    add site_id to ci config settings

--- backend/config/settings/ci.py
+++ backend/config/settings/ci.py
@@ -3,7 +3,7 @@ from .base import *  # noqa: F401, F403
 
 SECRET_KEY = "Cu96ky2qXk9yxxd4CrDOlQpFrCTJNl31sxtxBhEfpVBDxe6SGOlkUUuFT7nqk654"
 
-
+SITE_ID = 3
 ALLOWED_HOSTS = ["*"]
 DEBUG = True
 DATABASES = {
