4fef4d9f3e204b30dde1f5a01955bc6c5da18452
Christophe Dupin <mail.christophe.dupin@gmail.com>
2021-03-14 12:53:19 +0100

    test

--- backend/app/posts/adapter.py
+++ backend/app/posts/adapter.py
@@ -4,12 +4,17 @@ from app.users.models import User
 
 class MySocialAccountAdapter(DefaultSocialAccountAdapter):
     def pre_social_login(self, request, sociallogin):
+
         user = sociallogin.user
         if user.id:
             return
+        if not user.email:
+            return
+
         try:
-            existing_user = User.objects.get(email=user.email)
-            sociallogin.state["process"] = "connect"
-            perform_login(request, existing_user, "none")
+            user = User.objects.get(
+                email=user.email
+            )  # if user exists, connect the account to the existing account and login
+            sociallogin.connect(request, user)
         except User.DoesNotExist:
             pass
