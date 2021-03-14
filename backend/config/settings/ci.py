3d5ca5e67c943062092219fec05424af0b0cd8ac
Christophe Dupin <mail.christophe.dupin@gmail.com>
2021-03-14 11:16:29 +0100

    avoid error xith secret key

--- backend/config/settings/ci.py
+++ backend/config/settings/ci.py
@@ -1,10 +1,9 @@
 import os
 from .base import *  # noqa: F401, F403
 
-SECRET_KEY = os.getenv(
-    "SECRET_KEY",
-    default="Cu96ky2qXk9yxxd4CrDOlQpFrCTJNl31sxtxBhEfpVBDxe6SGOlkUUuFT7nqk654",
-)
+SECRET_KEY = "Cu96ky2qXk9yxxd4CrDOlQpFrCTJNl31sxtxBhEfpVBDxe6SGOlkUUuFT7nqk654"
+
+
 ALLOWED_HOSTS = ["*"]
 DEBUG = True
 DATABASES = {
